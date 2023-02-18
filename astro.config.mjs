92f5d81731f1f92efa632de76f1824b04a58ab7a
a-poor <45295232+a-poor@users.noreply.github.com>
2023-02-18 12:54:04 -0800

    Add meta tags for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://austinpoor.com/",
   integrations: [
     tailwind(), 
     mdx({
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,46 @@
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+
+
+export async function get(context: APIContext) {
+    const blogPosts = await getCollection('blog');
+    return rss({
+        // `<title>` field in output xml
+        title: "Austin's Blog",
+
+        // `<description>` field in output xml
+        description: "A humble Astronaut's guide to the stars",
+
+        // Pull in your project "site" from the endpoint context
+        // https://docs.astro.build/en/reference/api-reference/#contextsite
+        site: context.site?.toString() || 'https://austinpoor.com',
+
+        // Array of `<item>`s in output xml
+        // See "Generating items" section for examples using content collections and glob imports
+        items: blogPosts
+            .filter(p => !p.data.isDraft)
+            .sort((a, b) => {
+                // Choose the most recent date (if "updated", use that, otherwise use "published")
+                const da = a.data.updateDate || a.data.publishDate;
+                const db = b.data.updateDate || b.data.publishDate;
+
+                // Sort ascending...
+                if (da === db) return 0;
+                return da < db ? -1 : 1;
+            })
+            .reverse()
+            .map((post) => ({
+                title: post.data.title,
+                pubDate: post.data.updateDate || post.data.publishDate,
+                description: post.data.description,
+                // customData: post.data.customData,
+                // Compute RSS link from post `slug`
+                // This example assumes all posts are rendered as `/blog/[slug]` routes
+                link: `/blog/${post.slug}/`,
+            })),
+
+        // (optional) inject custom xml
+        customData: `<language>en-us</language>`,
+    });
+}
