192ca3199e7ae08a47ee920dcb0fb764b074bad9
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-11-29 15:55:33 -0800

    Adds cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,25 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/static";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://austinpoor.com/",
   integrations: [
     tailwind(), 
     mdx({
-      drafts: true
+      drafts: true,
     }), 
     prefetch({
-      selector: "a[href^='/blog']"
-    }),
-    sitemap(), 
+      selector: "a[href^='/blog']",
+    }), 
+    sitemap(),
   ],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: process.env.NODE_ENV === "production",
-    },
-    speedInsights: {
-      enabled: process.env.NODE_ENV === "production",
-    },
+  output: "server",
+  adapter: cloudflare({
+    mode: "directory",
   })
 });
