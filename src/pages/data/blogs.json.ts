6c1cca51cd76ebf317d6a99fc4115eefc3e80bc7
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-05-24 21:29:08 -0700

    Updates blog json data

--- src/pages/data/blogs.json.ts
+++ src/pages/data/blogs.json.ts
@@ -2,7 +2,10 @@ import { getCollection } from 'astro:content';
 
 export async function GET() {
     const entries = await getCollection('blog');
-    const data = entries.filter(d => !d.data.isDraft)
+    const data = entries
+        .filter(d => !d.data.isDraft)
+        .sort((a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime())
+        ;
     return new Response(
         JSON.stringify(data),
         {
