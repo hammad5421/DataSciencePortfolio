98f4acfbafa27bd6e9b0834b335a691f4ec65b47
Hammad Ahmad <ha4121221@gmail.com>
2024-08-25 19:02:59 +0500

    initial changes done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
-
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: "server",
-  site: "https://austinpoor.com/",
-  prefetch: true,
+  // output: 'server',
+  // site: 'http://localhost:3000',
+  // prefetch: true,
   integrations: [
     tailwind(),
     sitemap(),
   ],
-  adapter: cloudflare(),
-  image: {
-    service: passthroughImageService(),
-  },
+  // adapter: cloudflare(),
 });
